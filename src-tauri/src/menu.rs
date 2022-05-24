269de196bbb7bc88fb9f7c0a6bee6ee2d2c36e13
lencx <cxin1314@gmail.com>
2022-05-25 01:09:20 +0800

    chore: build

--- src-tauri/src/menu.rs
+++ src-tauri/src/menu.rs
@@ -1,7 +1,12 @@
 use tauri::{Menu, MenuItem, Submenu};
 
 pub fn init() -> Menu {
-    let quit = Submenu::new("", Menu::new().add_native_item(MenuItem::Quit));
+    let ctx = tauri::generate_context!();
+
+    let quit = Submenu::new(
+        &ctx.package_info().name,
+        Menu::with_items([MenuItem::Hide.into(), MenuItem::Quit.into()]),
+    );
     let menu = Menu::new().add_submenu(quit);
 
     menu
