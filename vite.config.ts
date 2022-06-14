bbc26a38c10db3a0ca2f3cb4fce92672b8a79a55
lencx <cxin1314@gmail.com>
2022-06-15 00:13:51 +0800

    chore: rsw

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import Unocss from 'unocss/vite';
+import ViteRsw from 'vite-plugin-rsw';
 
 import unoOptions from './unocss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    ViteRsw(),
     Unocss(unoOptions),
     tsconfigPaths(),
     react(),
