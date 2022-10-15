aec088d20a7597222037e39035261aaf152920ee
lencx <cxin1314@gmail.com>
2022-10-15 16:03:24 +0800

    chore: readme

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,12 +7,19 @@ mod omb;
 
 fn main() {
     let context = tauri::generate_context!();
-    tauri::Builder::default()
+    let app = tauri::Builder::default()
         .setup(omb::setup::init)
         .plugin(omb::fs::FsExtra::default())
         .menu(tauri::Menu::os_default(&context.package_info().name))
         .system_tray(omb::tray::menu())
         .on_system_tray_event(omb::tray::handler)
-        .run(context)
+        .build(context)
         .expect("error while running OhMyBox application");
+
+    app.run(|_app_handle, event| match event {
+        tauri::RunEvent::Updater(updater_event) => {
+            dbg!(updater_event);
+        }
+        _ => {}
+    })
 }
